b45bf47012ee7c20f181a190dc7270fe72859b17
Samuel Mancini (4BHEL) <samuel.mancini@htl-klu.at>
2021-10-14 13:01:21 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Resistor from "./component/Resistor";
+import {useState} from "react";
 
 function App() {
+  const [value1, changeValue1] = useState(0);
+  const [value2, changeValue2] = useState(0);
+  const [value3, changeValue3] = useState(0);
   return (
     <div className="App">
     <h1>Widerstandsberechnung</h1>
-      <Resistor title = "R1"/>
-      <Resistor title = "R2"/>
-      <Resistor title = "R3"/>
+      <Resistor title = "R1" handleInput ={(value) => changeValue1(value)}/>
+      <Resistor title = "R2" handleInput ={(value) => changeValue2(value)}/>
+      <Resistor title = "R3" handleInput ={(value) => changeValue3(value)}/>
+    <button>Serienschaltung</button>
+      <button>Paralellschaltung</button>
     </div>
   );
 }
--- src/component/Resistor.js
+++ src/component/Resistor.js
@@ -1,10 +1,13 @@
+import {useState} from "react";
 
 function Resistor(props) {
     return (
+        <div>
         <label>
             {props.title}:
-    <input type = "number"/>
+    <input type = "number" onChange={props.handleInput}/>
     </label>
+    </div>
     );
 }
 export default Resistor;
