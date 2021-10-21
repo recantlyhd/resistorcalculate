f49cd0d08ace768d35c81702706fca9aeebf62b6
Samuel Mancini (4BHEL) <samuel.mancini@htl-klu.at>
2021-10-21 13:04:06 +0200

    added calculate methods for I

--- src/App.js
+++ src/App.js
@@ -3,44 +3,54 @@ import Resistor from "./component/Resistor";
 import Serialconnection from "./component/Serialconnection";
 import {useState} from "react";
 import Parallelconnection from "./component/Parallelconnection";
+import './component/button.css';
 
 function App() {
-  const [value1, changeValue1] = useState(0);
-  const [value2, changeValue2] = useState(0);
-  const [value3, changeValue3] = useState(0);
-  const [sResistorValue, changeSResistorValue] = useState(0);
-  const [pResistorValue, changePResistorValue] = useState(0);
-
-
+    const [value1, changeValue1] = useState(0);
+    const [value2, changeValue2] = useState(0);
+    const [value3, changeValue3] = useState(0);
+    const [sResistorValue, changeSResistorValue] = useState(0);
+    const [pResistorValue, changePResistorValue] = useState(0);
+    const [result, setResult] = useState(0);
+    
+    function calculateV(volts, ohm){
+        return ohm/volts;
+    }
     function calculateResistorSerial() {
-      const res = value1 + value2 + value3;
-    changeSResistorValue(res);
-    return (
-        <div>
-            {sResistorValue}
-        </div>
-    );
-  }
-    function calculateResistorParallel() {
-      const res = (value1 * value2 * value3)/(value1 + value2 + value3);
-        changePResistorValue(res);
-      return(
-          <div>
-              {pResistorValue}
-          </div>
-      )
+        changePResistorValue(0);
+        const res = value1 + value2 + value3;
+        changeSResistorValue(res);
+        return (
+            <div>
+                {sResistorValue}
+            </div>
+        );
     }
 
-  return (
-      <div className="App">
-        <h1>Widerstandsberechnung</h1>
-        <Resistor title="R1" handleInput={(value) => changeValue1(parseInt(value))}/>
-        <Resistor title="R2" handleInput={(value) => changeValue2(parseInt(value))}/>
-        <Resistor title="R3" handleInput={(value) => changeValue3(parseInt(value))}/>
-          <Serialconnection onClick = {calculateResistorSerial} result = {sResistorValue}/>
-          <Parallelconnection onClick = {calculateResistorParallel} result = {pResistorValue}/>
-      </div>
-  );
+    function calculateResistorParallel() {
+        changeSResistorValue(0);
+        const res = (value1 * value2 * value3) / (value1 + value2 + value3);
+        changePResistorValue(res);
+        return (
+            <div>
+                {pResistorValue}
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>Widerstandsberechnung</h1>
+            <Resistor title="R1" handleInput={(value) => changeValue1(parseInt(value))}/>
+            <Resistor title="R2" handleInput={(value) => changeValue2(parseInt(value))}/>
+            <Resistor title="R3" handleInput={(value) => changeValue3(parseInt(value))}/>
+            <div>
+                <Serialconnection className = 'app-button' onClick={calculateResistorSerial} result={sResistorValue}/>
+            <Parallelconnection className = 'app-button' onClick={calculateResistorParallel} result={pResistorValue}/>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/component/Serialconnection.js
+++ src/component/Serialconnection.js
@@ -1,9 +1,8 @@
-
 function Serialconnection(props){
     return(
         <div>
-            <button onClick = {props.onClick}>Serienschaltung</button>
-            {props.result != 0 ? <h1>R seriell: {props.result}</h1> : null}
+        <button onClick = {props.onClick}>Serienschaltung</button>
+            {props.result != 0 ?<h1>R seriell: {props.result}</h1> : null}
         </div>
             );
 }
